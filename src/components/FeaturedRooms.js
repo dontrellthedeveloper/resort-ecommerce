978eae181ce229add83c1c846e46dc020990c753
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-09-23 18:12:52 -0700

    added loading component setup

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { RoomContext } from '../context';
+import Loading from './Loading';
+import Room from './Room';
 
 export default class FeaturedRooms extends Component {
     static contextType = RoomContext;
@@ -9,7 +11,8 @@ export default class FeaturedRooms extends Component {
 
         return (
             <div>
-            
+            <Room/>
+            <Loading/>
             </div>
         )
     }
